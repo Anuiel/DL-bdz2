9b47a80f56f9daa7e419a01dfff7d8d019be4a63
Max <n00t-noot@yandex.ru>
2024-03-11 17:25:13 +0300

    bigger model

--- src/config.py
+++ src/config.py
@@ -9,11 +9,11 @@ class Config:
     source_language: str = 'de'
     target_language: str = 'en'
 
-    embedding_size: int = 512
+    embedding_size: int = 1024
     number_of_heads: int = 8
-    feed_forward_hidden_size: int = 1024
-    encoder_layers_num: int = 3
-    decoder_layer_num: int = 3
+    feed_forward_hidden_size: int = 2048
+    encoder_layers_num: int = 5
+    decoder_layer_num: int = 5
     dropout_rate: float = 0.1
 
     n_epoch: int = 12
